0143e29415680717346fd39a9fa2b53e2336c579
Clispy1 <clispy1@gmail.com>
2025-08-26 19:25:41 +0000

    Fetch Companion Library

---
+++ components/Navbar.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import NavItems from "./NavItems";
+
+import {
+    SignInButton,
+    SignUpButton,
+    SignedIn,
+    SignedOut,
+    UserButton,
+} from "@clerk/nextjs";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <Link href="/">
+                <div className="flex items-center gap-2.5 cursor-pointer">
+                    <Image
+                        src="/images/logo.svg"
+                        alt="logo"
+                        width={45}
+                        height={45}
+                    />
+                </div>
+            </Link>
+
+            <div className="flex items-center gap-8">
+                <NavItems />
+                <SignedOut>
+                    <SignInButton>
+                        <button className="btn-signin">Sign In</button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
