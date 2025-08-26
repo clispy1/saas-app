c3ef7dd803305ae45de5ba63f5b42041685ed430
Clispy1 <clispy1@gmail.com>
2025-08-26 18:35:07 +0000

    Implement Auth, Billing, Database Setup, Create Companion Server Action

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,50 @@
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import { recentSessions } from "@/constants";
+
 const Page = () => {
-    return <div>Page</div>;
+    return (
+        <main>
+            <h1 className="text-2xl underline">Popular Companions</h1>
+
+            <section className="home-section">
+                <CompanionCard
+                    id="123"
+                    name="English"
+                    topic="English Language"
+                    subject="Learning English Language"
+                    duration={45}
+                    color="#ffda6e"
+                />
+                <CompanionCard
+                    id="456"
+                    name="English"
+                    topic="English Language"
+                    subject="Learning English Language"
+                    duration={45}
+                    color="#ffda6e"
+                />
+                <CompanionCard
+                    id="789"
+                    name="English"
+                    topic="English Language"
+                    subject="Learning English Language"
+                    duration={45}
+                    color="#ffda6e"
+                />
+            </section>
+
+            <section className="home-section">
+                <CompanionsList
+                    title="Recently Completed Sessions"
+                    companions={recentSessions}
+                    classNames="w-2/3 max-lg:w-full"
+                />
+                <CTA />
+            </section>
+        </main>
+    );
 };
 
 export default Page;
